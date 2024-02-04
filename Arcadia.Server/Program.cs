5a806895bd7ffd21dc68fdc06a49ae7bfa605717
Charles Zhang <szinubuntu@gmail.com>
2024-02-04 09:06:13 -0500

    Add project files.

---
+++ Arcadia.Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arcadia.Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Arcadia
+
+Simple-minded websocket message-based anyone-is-welcome server.
